4787cbc5a4070d3f8a84cc0f122df14cfcdd339f
Kishan T A <kishantashok@gmail.com>
2024-12-15 20:01:51 +0530

    Try hosting in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:8989,
-  }
+  server: {
+    port: 8989, 
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for assets inside the output directory
+    rollupOptions: {
+      input: {
+        main: './index.html', // Entry point 
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@assets': '/src/assets', // Alias for easier imports of assets
+    },
+  },
 })
+
